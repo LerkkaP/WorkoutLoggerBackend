bd7d587ae77d2fec467dc082730d03824b5578a7
Erik Peteri <peteri.erik@gmail.com>
2023-09-12 16:21:31 +0300

    continue updating controllers

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -63,61 +63,43 @@ const addExerciseToWorkout = async (req, res) => {
 
 // add a new set to a specific exercise inside a specific workout
 const addSetToExercise = async (req, res) => {
-  const workout_id = req.params.workout_id;
-  const exercise_id = req.params.exercise_id;
-  const { reps, load } = req.body;
+  try {
+    const workout_id = req.params.workout_id;
+    const exercise_id = req.params.exercise_id;
+    const { reps, load } = req.body;
 
-  console.log(reps, load);
+    const updatedExercise = await Workout.findOneAndUpdate(
+      { _id: workout_id, "exercises._id": exercise_id },
+      { $push: { "exercises.$.sets": { reps, kg: load } } },
+      { new: true }
+    );
 
-  const updatedExercise = await Workout.findOneAndUpdate(
-    { _id: workout_id, "exercises._id": exercise_id },
-    {
-      $push: {
-        "exercises.$.sets": { sets: { reps, load } },
-      },
-    }
-  );
-
-  res.status(201).json(updatedExercise);
-
-  /*const workout = workouts.find((workout) => workout.id === workout_id);
-
-  if (!workout) {
-    return res.status(404).json({ error: "Workout not found" });
+    res.status(200).json(updatedExercise);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-
-  const exercise = workout.exercises.find((e) => e.exercise_id === exercise_id);
-
-  if (!exercise) {
-    return res.status(404).json({ error: "Exercise not found" });
-  }
-
-  const newSet = {
-    reps: parseFloat(reps),
-    kg: parseFloat(load),
-  };
-
-  exercise.sets.push(newSet);
-
-  res.json(workout);*/
 };
 
 // delete a specific exercise inside a specific workout
-const deleteExerciseFromWorkout = (req, res) => {
-  const workout_id = Number(req.params.workout_id);
-  const exercise_id = req.params.exercise_id;
+const deleteExerciseFromWorkout = async (req, res) => {
+  try {
+    const workout_id = req.params.workout_id;
+    const exercise_id = req.params.exercise_id;
 
-  const workout = workouts.find((workout) => workout.id === workout_id);
+    await Workout.findOneAndUpdate(
+      { _id: workout_id },
+      { $pull: { exercises: { _id: exercise_id } } }
+    );
 
-  if (!workout) {
-    return res.status(404).json({ error: "Workout not found" });
+    // look into this !!! //
+
+    if (Workout.exercises.length === 0) {
+      await Workout.findByIdAndDelete(workout_id);
+    }
+    res.status(200).json();
+  } catch (error) {
+    res.json(500).json({ error: error.message });
   }
-
-  workout.exercises = workout.exercises.filter(
-    (exercise) => exercise.exercise_id !== exercise_id
-  );
-
-  res.status(204).end();
 };
 
 // delete a specific set inside a specific exercise
