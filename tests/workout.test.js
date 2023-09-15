b5915dabaf232481735d589c7bd25e564c4afabd
Erik Peteri <peteri.erik@gmail.com>
2023-09-15 19:34:49 +0300

    configure testing environment and write tests

--- tests/workout.test.js
+++ tests/workout.test.js
@@ -1,16 +1,32 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../app");
+const Workout = require("../models/Workout");
+const workouts = require("../data");
+
+beforeEach(async () => {
+  await Workout.deleteMany({});
+  let workoutObject = new Workout(workouts[0]);
+  await workoutObject.save();
+  workoutObject = new Workout(workouts[1]);
+  await workoutObject.save();
+});
 
 describe("GET /workouts", () => {
-  test("should return all workouts", async () => {
+  test("should return type json", async () => {
     await request(app)
       .get("/workouts")
       .expect(200)
       .expect("Content-Type", /application\/json/);
   });
+
+  test("should return all workouts", async () => {
+    const res = await request(app).get("/workouts");
+
+    expect(res.body).toHaveLength(workouts.length);
+  });
 });
 
-afterEach(async () => {
+afterAll(async () => {
   await mongoose.connection.close();
 });
