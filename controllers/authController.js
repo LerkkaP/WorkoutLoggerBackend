aa085fbd5ff09b7f63bdd54006007cc0379b4a58
Erik Peteri <peteri.erik@gmail.com>
2023-11-01 17:53:12 +0200

    implement functionality for creating account

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,33 @@
-//const User = require("../models/User");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const signUp = (req, res) => {
-  console.log(req.body);
+const signUp = async (req, res) => {
+  const { username, password1, password2 } = req.body;
+
+  const existingUser = await User.findOne({ username });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  if (password1 !== password2) {
+    return res.status(400).json({ message: "Passwords do not match." });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password1, saltRounds);
+
+    const newUser = new User({
+      username,
+      hash_password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Account created successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const signIn = (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
   },
-  password: {
+  hash_password: {
     type: String,
     required: true,
+    trim: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
   },
 });
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.hash_password);
+};
 
 const User = mongoose.model("User", userSchema);
 
