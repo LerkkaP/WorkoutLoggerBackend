e3da820af06e16ab178e7d199166aaf87c8fb5e3
Erik Peteri <peteri.erik@gmail.com>
2023-11-01 16:22:03 +0200

    create route for authentication

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require("express")
-const config = require("./utils/config")
-const mongoose = require("mongoose")
-const workout_routes = require("./routes/workouts")
-const morgan = require("morgan")
-const helmet = require("helmet")
-var cors = require("cors")
+const express = require("express");
+const config = require("./utils/config");
+const mongoose = require("mongoose");
+const workout_routes = require("./routes/workouts");
+const auth_routes = require("./routes/auth");
+const morgan = require("morgan");
+const helmet = require("helmet");
+var cors = require("cors");
 
 mongoose.connect(config.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(morgan("common"))
-app.use(helmet())
-app.use(cors())
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+app.use(cors());
 
 app.use("/workouts", workout_routes);
+app.use("/auth", auth_routes);
 
 module.exports = app;
---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+//const User = require("../models/User");
+
+const signUp = (req, res) => {
+  console.log(req.body);
+};
+
+const signIn = (req, res) => {
+  console.log(req.body);
+};
+
+module.exports = { signUp, signIn };
